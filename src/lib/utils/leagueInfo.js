fdee838f70f44f7228f0906ca076330ec4f57c4f
joebafett19 <89045060+joebafett19@users.noreply.github.com>
2021-08-16 13:45:28 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "your_league_id"; // your league ID
-export const leagueName = "your_league_name"; // your league name
+export const leagueID = "687455289808592896"; // your league ID
+export const leagueName = "The League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 
