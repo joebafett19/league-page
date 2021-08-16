18ff2b24a33eb8e51055263bd2df7b99a42d03fe
joebafett19 <89045060+joebafett19@users.noreply.github.com>
2021-08-16 14:26:24 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -26,7 +26,7 @@ export const managers = [
       "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "San Diego, CA", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "/managers/20210816_141410.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/shiva1.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2010, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
